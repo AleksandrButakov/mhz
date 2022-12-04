3f69f2b319d6d9f3ec48d1f370172eb33ccca193
Aleksandr Butakov <anbngm@gmail.com>
2022-12-04 19:58:20 +0400

    Added webView layout

--- app/src/main/java/ru/anbn/mhz/MainActivity.java
+++ app/src/main/java/ru/anbn/mhz/MainActivity.java
@@ -9,11 +9,21 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
-import android.widget.Spinner;
+import android.widget.ListView;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     private static String url;
+
+    /* алгоритм работает следующим образом. Кликаем в TextView, возвращаем номер выбранной строки.
+    По номеру строки находим в массиве индексы оборудования и платы, описание которого и выводим
+    в pdfViewer.
+     */
+
+    // массив для дальнейшего заполнения найденными позициями
+    public ArrayList listCardArray = new ArrayList();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,9 +35,9 @@ public class MainActivity extends AppCompatActivity {
 
 
         // зададим идентификаторы полям spinner
-        final Spinner spinner_road = findViewById(R.id.spinner_road);
-        final Spinner spinner_region = findViewById(R.id.spinner_region);
-        final Spinner spinner_station = findViewById(R.id.spinner_station);
+//        final Spinner spinner_road = findViewById(R.id.spinner_road);
+//        final Spinner spinner_region = findViewById(R.id.spinner_region);
+//        final Spinner spinner_station = findViewById(R.id.spinner_station);
 
 
         String[] sRoad = {
@@ -69,25 +79,24 @@ public class MainActivity extends AppCompatActivity {
         };
 
         // адаптер для spinner_road со списком дорог
-        final ArrayAdapter<String> adapter_road = new ArrayAdapter<String>(this, android.
-                R.layout.simple_spinner_item, sRoad);
-        adapter_road.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner_road.setAdapter(adapter_road);
-
-        // адаптер для spinner_region со списком регионов
-        final ArrayAdapter<String> adapter_region = new ArrayAdapter<String>(this, android.
-                R.layout.simple_spinner_item, sRegion);
-        adapter_region.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner_region.setAdapter(adapter_region);
-
-        // адаптер для spinner_station со списком станций
-        final ArrayAdapter<String> adapter_station = new ArrayAdapter<String>(this, android.
-                R.layout.simple_spinner_item, sStation);
-        adapter_station.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner_station.setAdapter(adapter_station);
+//        final ArrayAdapter<String> adapter_road = new ArrayAdapter<String>(this, android.
+//                R.layout.simple_spinner_item, sRoad);
+//        adapter_road.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+//        spinner_road.setAdapter(adapter_road);
+//
+//        // адаптер для spinner_region со списком регионов
+//        final ArrayAdapter<String> adapter_region = new ArrayAdapter<String>(this, android.
+//                R.layout.simple_spinner_item, sRegion);
+//        adapter_region.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+//        spinner_region.setAdapter(adapter_region);
+//
+//        // адаптер для spinner_station со списком станций
+//        final ArrayAdapter<String> adapter_station = new ArrayAdapter<String>(this, android.
+//                R.layout.simple_spinner_item, sStation);
+//        adapter_station.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+//        spinner_station.setAdapter(adapter_station);
 
     }
-
 
     // нарисуем меню
     @Override
@@ -119,7 +128,6 @@ public class MainActivity extends AppCompatActivity {
             Intent intent = new Intent(Intent.ACTION_VIEW);
             intent.setData(Uri.parse("https://play.google.com/store/apps/details?id=com.anbn.pinout"));
             startActivity(intent);
-
         }
 
         // о программе
@@ -128,8 +136,31 @@ public class MainActivity extends AppCompatActivity {
             Intent intent = new Intent(this, WebViewActivity.class);
             startActivity(intent);
         }
-
         return true;
     }
 
+
+
+
+    // очистка содержимого в ListView
+    public void ListViewClear() {
+        // получаем экземпляр элемента ListView
+        ListView listView = findViewById(R.id.listView);
+        // очистим listArray для дальнейшей очистки массива
+        listCardArray.clear();
+        // используем адаптер данных
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, listCardArray);
+        listView.setAdapter(adapter);
+    }
+
+    // вывод результатов поиска в ListView
+    public void SearchResultsDisplay() {
+        // получаем экземпляр элемента ListView
+        ListView listView = findViewById(R.id.listView);
+        // используем адаптер данных
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, listCardArray);
+        listView.setAdapter(adapter);
+    }
 }

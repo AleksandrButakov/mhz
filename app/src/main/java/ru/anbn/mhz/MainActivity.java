b4dbfda450ca3a0131878b7109510ab1eb70bc66
Aleksandr Butakov <anbngm@gmail.com>
2022-12-07 16:33:08 +0400

    Added method read file

--- app/src/main/java/ru/anbn/mhz/MainActivity.java
+++ app/src/main/java/ru/anbn/mhz/MainActivity.java
@@ -1,8 +1,11 @@
 package ru.anbn.mhz;
 
+import android.app.DownloadManager;
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -12,8 +15,8 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
+import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 
@@ -21,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
     // массив для дальнейшего заполнения найденными позициями
     public ArrayList listCardArray = new ArrayList();
 
-    private final static String FILE_NAME = "content.txt";
+    private static String FILE_NAME = "content.txt";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -81,12 +84,27 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onClickButton2(View view) throws IOException {
-        openText();
+        // openText();
+        saveFile();
+
     }
 
     // сохранение файла
-    public void saveText(){
+    public void saveText() {
 
+        /*
+        EditText editText1 = findViewById(R.id.editText1);
+        editText1.setText(Environment.getExternalStorageDirectory().getAbsolutePath());
+        */
+
+        String filePath = Environment.getExternalStorageDirectory().getAbsolutePath() + "/Download/mhz_data.txt";
+        File file = new File(filePath);
+        if (file.exists()) {
+            file.delete();
+        }
+
+
+        /*
         FileOutputStream fos = null;
         try {
             EditText editText1 = findViewById(R.id.editText1);
@@ -95,57 +113,104 @@ public class MainActivity extends AppCompatActivity {
             fos = openFileOutput(FILE_NAME, MODE_APPEND);
             fos.write(text.getBytes());
             Toast.makeText(this, "Файл сохранен", Toast.LENGTH_SHORT).show();
-        }
-        catch(IOException ex) {
+        } catch (IOException ex) {
 
             Toast.makeText(this, ex.getMessage(), Toast.LENGTH_SHORT).show();
-        }
-        finally{
-            try{
-                if(fos!=null)
+        } finally {
+            try {
+                if (fos != null)
                     fos.close();
-            }
-            catch(IOException ex){
+            } catch (IOException ex) {
 
                 Toast.makeText(this, ex.getMessage(), Toast.LENGTH_SHORT).show();
             }
         }
+        */
+
     }
 
     // открытие файла
-    public void openText(){
-
+    public void openText() {
+        FILE_NAME = "https://drive.google.com/uc?export=download&confirm=no_antivirus&id=1u9-qgZsTMSruygixHCJZvLbtlG2QWWmS";
         FileInputStream fin = null;
         EditText editText2 = findViewById(R.id.editText2);
         try {
             fin = openFileInput(FILE_NAME);
             byte[] bytes = new byte[fin.available()];
             fin.read(bytes);
-            String text = new String (bytes);
+            String text = new String(bytes);
             editText2.setText(text);
-        }
-        catch(IOException ex) {
+        } catch (IOException ex) {
 
             Toast.makeText(this, ex.getMessage(), Toast.LENGTH_SHORT).show();
-        }
-        finally{
+        } finally {
 
-            try{
-                if(fin!=null)
+            try {
+                if (fin != null)
                     fin.close();
-            }
-            catch(IOException ex){
+            } catch (IOException ex) {
 
                 Toast.makeText(this, ex.getMessage(), Toast.LENGTH_SHORT).show();
             }
         }
     }
 
+
     // скопируем файл из сети
     // https://drive.google.com/file/d/1u9-qgZsTMSruygixHCJZvLbtlG2QWWmS/view?usp=sharing
+    private void saveFile() {
+
+        // путь к файлу на google drive
+        String link = "https://drive.google.com/uc?export=download&confirm=no_antivirus&id=1u9-qgZsTMSruygixHCJZvLbtlG2QWWmS";
+        // путь к файлу на external drive
+        String filePath = android.os.Environment.getExternalStorageDirectory().getAbsolutePath() + "/Download/mhz_data.txt";
+
+        DownloadManager.Request request = new DownloadManager.Request(Uri.parse(link));
+        request.setAllowedNetworkTypes(DownloadManager.Request.NETWORK_MOBILE | DownloadManager.Request.NETWORK_WIFI);
+        request.setTitle("Download...");
+        request.setDescription("File is download...");
+
+        File file = new File(filePath);
+        request.allowScanningByMediaScanner();
+        request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE | DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+        request.setDestinationUri(Uri.fromFile(file));
+        DownloadManager manager = (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
+        manager.enqueue(request);
 
 
+        Toast.makeText(this, "File uploaded successfully!", Toast.LENGTH_SHORT).show();
+
+        // удаляем загруженный файл из папки download
+        if (file.exists()) {
+            file.delete();
+        }
+
+/*
+        // открытие файла
+        FileInputStream fin = null;
+        EditText editText2 = findViewById(R.id.editText2);
+        try {
+            fin = openFileInput(filePath + ".txt");
+            byte[] bytes = new byte[fin.available()];
+            fin.read(bytes);
+            String text = new String(bytes);
+            editText2.setText(text);
+        } catch (IOException ex) {
+
+            Toast.makeText(this, ex.getMessage(), Toast.LENGTH_SHORT).show();
+        } finally {
+
+            try {
+                if (fin != null)
+                    fin.close();
+            } catch (IOException ex) {
+
+                Toast.makeText(this, ex.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        }
+        */
 
 
+    }
 
 }

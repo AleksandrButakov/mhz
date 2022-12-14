e3339403335381c3d356a68f2f573caa72b48e3f
Aleksandr Butakov <anbngm@gmail.com>
2022-12-14 19:13:45 +0400

    Redesign

--- app/src/main/java/ru/anbn/mhz/MainActivity.java
+++ app/src/main/java/ru/anbn/mhz/MainActivity.java
@@ -67,7 +67,6 @@ public class MainActivity extends AppCompatActivity {
     private boolean bSearch;
 
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);

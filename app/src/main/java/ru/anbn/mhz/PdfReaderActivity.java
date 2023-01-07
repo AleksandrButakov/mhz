1c804f160ab1d5dafd5f1e5b4edf6e971e999480
Aleksandr Butakov <anbngm@gmail.com>
2023-01-07 19:13:09 +0500

    changed class pdfReader

--- app/src/main/java/ru/anbn/mhz/PdfReaderActivity.java
+++ app/src/main/java/ru/anbn/mhz/PdfReaderActivity.java
@@ -3,9 +3,7 @@ package ru.anbn.mhz;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -14,6 +12,8 @@ import com.github.barteksc.pdfviewer.util.FitPolicy;
 
 // extends AppCompatActivity необходим для загрузки файлов
 public class PdfReaderActivity extends AppCompatActivity {
+
+    String path;
 
     // нарисуем экран
     @Override
@@ -27,25 +27,20 @@ public class PdfReaderActivity extends AppCompatActivity {
         // pdf Viewer
         PDFView pdfView = findViewById(R.id.pdfView);
 
-        String folder = "";
-        String file_name = "";
-        String path;
+
 
         // выведем выбранную станцию и частоту на экран
-        TextView textView = findViewById(R.id.textView);
+        TextView textView = findViewById(R.id.textView1);
         if (!MainActivity.choiceFrequency.equals("")) {
             // станция выбрана
             textView.setText(MainActivity.choiceFrequency);
-        } else {
-            // станция не выбрана
-            textView.setText("Выберите станцию для отображения частоты");
-        }
+        } //else {
+//            // станция не выбрана
+//            textView.setText("Выберите станцию для отображения частоты");
+//        }
 
         // найдем путь к файлу
         path = MainActivity.fileName;
-
-
-
 
         pdfView.fromAsset(path)
                 //.pages(35) // all pages are displayed by default
@@ -64,7 +59,6 @@ public class PdfReaderActivity extends AppCompatActivity {
                 .pageFitPolicy(FitPolicy.WIDTH) // mode to fit pages in the view
                 // scaled relative to largest page.
                 .load();
-        //}
     }
 
     // для стрелки назад в ActionBar
@@ -84,20 +78,20 @@ public class PdfReaderActivity extends AppCompatActivity {
         return true;
     }
 
-    public void onClickButtonTwo(View v) {
-        //Intent intent = new  Intent(this, MainActivity.class);
-        //startActivity(MainActivity.class);
-        onBackPressed();// возврат на предыдущий activity
-    }
+//    public void onClickButtonTwo(View v) {
+//        //Intent intent = new  Intent(this, MainActivity.class);
+//        //startActivity(MainActivity.class);
+//        onBackPressed();// возврат на предыдущий activity
+//    }
     //"#FF9800"
 
     // дальнейший фрагмент выполняет загрузку файла по нажатию кнопки Download
-    public void onClickButtonDownload(View v) {
-        //
-        Toast.makeText(this, "Функционал находится в разработке...",
-                Toast.LENGTH_SHORT).show();
-        //DefineVersion();
-    }
+//    public void onClickButtonDownload(View v) {
+//        //
+//        Toast.makeText(this, "Функционал находится в разработке...",
+//                Toast.LENGTH_SHORT).show();
+//        //DefineVersion();
+//    }
 
 
 }
